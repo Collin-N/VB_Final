d0dc59e55ab735e46dfceb0cf9621db6af8ea449
Christian Snyder <Csnyder0920@gmail.com>
2017-12-13 21:06:36 -0600

    Update: Added AlienInvaderz

--- VBFinal/VBFinal/MainMenu.vb
+++ VBFinal/VBFinal/MainMenu.vb
@@ -1,16 +1,16 @@
 ﻿Public Class MainMenu
 
-    Private Sub btnFalldown_Click(sender As Object, e As EventArgs) Handles btnFalldown.Click
-        Dim box = New Falldown()
-        box.Show()
-    End Sub
-
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
         Me.Close()
     End Sub
 
     Private Sub btnTicTacToe_Click(sender As Object, e As EventArgs) Handles btnTicTacToe.Click
         Dim box = New TicTacToe()
+        box.Show()
+    End Sub
+
+    Private Sub btnInvaderz_Click(sender As Object, e As EventArgs) Handles btnInvaderz.Click
+        Dim box = New AlienInvaderz()
         box.Show()
     End Sub
 End Class
